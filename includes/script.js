462369bf03bd60d2b5a999fd4580a28613521710
alarn777 <michaelrokitko@Michaels-MacBook-Pro.local>
2018-05-16 01:02:29 +0300

    2

--- includes/script.js
+++ includes/script.js
@@ -1,9 +1,26 @@
 
 
+//
+// var newObj = document.createElement("h1");
+// var formObj = document.getElementById("frm");   // father reference is saved to the variable
+// formObj.appendChild(newObj);   // add the son as the last son of the father
+// newObj.className = "msg";  //add class to the object created
+//
 
 
+//dynamic adding images to blank html file
+var ows = " oooo";
+function  cube() {
+    var newCube = document.createElement('div');
 
-if (hello)
-{
-    alert("wow");
+    var fatherObj = document.getElementById("addingPlace");   // father reference is saved to the variable
+    fatherObj.appendChild(newCube);   // add the son as the last son of the father
+    newCube.className = "cube_new" + ows;
+    ows += "o";
+    var opacity = 0.1 * Math.floor((Math.random() * 10) + 1);
+    newCube.style.opacity = opacity.toString();
+
 }
+
+
+
